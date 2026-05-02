bfeaae399b6277d286995386189a126c5a050e04
jurol <rollandjulien718@gmail.com>
2026-05-02 09:20:24 +0200

    Add bricks init

---
+++ colors.js
@@ -0,0 +1,4 @@
+export const colors = {
+  anger: "#f83b3a",
+  lightMustard: "#ebdf64",
+};
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { colors } from "./colors.js";
 // ==========================
 // STATE
 // ==========================
@@ -10,6 +11,10 @@ const timerEl = document.getElementById("timer");
 const pauseMenu = document.getElementById("pauseMenu");
 const continueBtn = document.getElementById("continueBtn");
 const restartBtn = document.getElementById("restartBtn");
+const gameBoard = document.getElementById("game-board");
+const WIDTH = 720;
+const HEIGHT = 540;
+const PADDLE_WIDTH = 100;
 
 let keys = {};
 
@@ -21,14 +26,39 @@ let animationId = null;
 
 // Game state
 let player = {
-  x: 100,
-  y: 800,
-  speed: 300,
+  x: 0,
+  y: -30,
+  speed: 600,
 };
 
 let score = 0;
 let lives = 3;
 let timeLeft = 60;
+
+let bricks = [
+  {
+    x: 30,
+    y: 30,
+    color: colors.anger,
+  },
+  {
+    x: 130,
+    y: 30,
+    color: colors.lightMustard
+  },
+  {
+    x: 230,
+    y: 30,
+  },
+  {
+    x: 330,
+    y: 30,
+  },
+  {
+    x: 430,
+    y: 30,
+  },
+];
 
 // ==========================
 // INPUT
@@ -63,10 +93,11 @@ function gameLoop(timestamp) {
 // ==========================
 function update(dt) {
   // Movement (smooth)
-  if (keys["ArrowLeft"]) player.x -= player.speed * dt;
-  if (keys["ArrowRight"]) player.x += player.speed * dt;
-  if (keys["ArrowUp"]) player.y -= player.speed * dt;
-  if (keys["ArrowDown"]) player.y += player.speed * dt;
+  if (keys["ArrowLeft"] && player.x > 0) player.x -= player.speed * dt;
+  if (keys["ArrowRight"] && player.x + PADDLE_WIDTH < WIDTH)
+    player.x += player.speed * dt;
+  // if (keys["ArrowUp"]) player.y -= player.speed * dt;
+  // if (keys["ArrowDown"]) player.y += player.speed * dt;
 
   // Timer
   timeLeft -= dt;
@@ -108,12 +139,22 @@ continueBtn.addEventListener("click", togglePause);
 // RESET / RESTART
 // ==========================
 function resetGame() {
-  player.x = 100;
-  player.y = 100;
+  player.x = 0;
+  player.y = 0;
 
   score = 0;
   lives = 3;
   timeLeft = 60;
+}
+
+function initBricks() {
+  bricks.map((brick) => {
+    const $brick = document.createElement("div");
+    $brick.classList.add("brick");
+    $brick.style.backgroundColor = brick.color
+    $brick.style.transform = `translate(${brick.x}px, ${brick.y}px)`;
+    gameBoard.append($brick);
+  });
 }
 
 restartBtn.addEventListener("click", () => {
@@ -134,6 +175,7 @@ function endGame() {
 // START
 // ==========================
 function startGame() {
+  initBricks();
   lastTime = performance.now();
   animationId = requestAnimationFrame(gameLoop);
 }
